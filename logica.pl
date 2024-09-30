820bd25d436b03c589d4223be284b02c3cb4a7d5
Celso Z. <celso.zacarias@estudante.ufjf.br>
2024-09-30 10:19:40 -0300

    Implementado predicado para retornar peça em determinada posição

--- logica.pl
+++ logica.pl
@@ -7,3 +7,17 @@ init_jogador2([[0, [2,8]],[0, [4,8]], [0, [6,8]], [0, [8,8]],
 	[0, [2,6]], [0, [4,6]], [0, [6,6]], [0, [8,6]]]).
 
 isOccupied([X,Y], Player_pieces) :- member([_, [X,Y]], Player_pieces).
+
+get_piece_at_position(Position, [], Piece).
+get_piece_at_position(Position, [[Dama , Position] | Rest], [Dama | [Position]]).
+get_piece_at_position(Position, [_ | Rest], Piece) :- get_piece_at_position(Position, Rest, Piece).
+
+
+
+
+
+/*movimentar([X,Y], Player1_pieces, Player2_pieces) :-
+	( not(isOccupied())
+	;
+	)*/
+	
